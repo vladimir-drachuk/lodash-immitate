9f75733d544168106e17114c6a147724b1b4d6dc
Vladimir Drachuk <vvdra4@gmail.com>
2020-01-21 01:44:05 +0200

    feat: add 4 func

--- lodash.js
+++ lodash.js
@@ -3,8 +3,8 @@ class Lodash {
         if (array instanceof Array) {
             if (typeof number !== 'number') number = 1;
             let current;
-            const result = array.map((item, i) => {
-                if (!i%number) {
+            let result = array.map((item, i) => {
+                if (!(i%number)) {
                     const arr = [];
                     arr.push(item);
                     current = arr;
@@ -13,11 +13,41 @@ class Lodash {
                     current.push(item);
                 }
             });
+            result = result.filter((item) => item !== undefined);
             return result;
         } else return [];
+    }
+
+    compact(array) {
+        return array.filter((item) => !!(item))
+    }
+
+    drop(array, number) {
+        if (number === undefined) number = 1;
+        if (!(array instanceof Array)) return [];
+        return array.filter((item,i) => i >= number);
+    }
+
+    dropWhile(array, predicate) {
+        let dropNumber = array.findIndex((item, i) => {
+            ((predicate(item, i, array)) && predicate(array[i+1], i+1, array));
+            console.log((predicate(item, i, array)));
+            return i;
+        });
+        if (!dropNumber) return array;
+        let droppedArray = this.drop(array, dropNumber + 1);
+        return droppedArray;
     }
 }
 
 const _$ = new Lodash();
 
-console.log(_$.chunk([1,2,3,4,5],2));
+var  users = [
+    {  'user' :  'barney' ,   'active' : false} ,
+    {  'user' :  'barney1' ,   'active' : false} ,
+    {  'user' :  'barney2' ,   'active' : false} ,
+    {  'user' :  'fred' ,     'active' : false} ,
+    {  'user' :  'pebbles' ,  'active' : true}
+  ];
+
+console.log(_$.dropWhile(users, function(o) { return !o.active; }));
